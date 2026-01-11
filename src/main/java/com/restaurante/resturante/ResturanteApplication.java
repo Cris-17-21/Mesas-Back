6c345bc0bbc3adf8f099acf7895f07b016d8dbbb
Cris-17-21 <cris.hbn17@gmail.com>
2026-01-11 13:03:26 -0500

    Inicializacion proyecto mesas - Backend

---
+++ src/main/java/com/restaurante/resturante/ResturanteApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.resturante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurante/resturante/ResturanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.resturante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
